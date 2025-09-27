9b015ddc30b5c2ee0927209a53e656d60414cabe
Alexia <alexia@shrekxia.local>
2025-09-27 15:37:38 +0300

    Rezolvare eroare

--- app/scan/page.js
+++ app/scan/page.js
@@ -1,7 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const ScanForm = dynamic(() => import("./ScanForm"), { ssr: false });
+const ScanForm = dynamic(() => import("./components/ScanForm"), { ssr: false });
 
 export default function ScanPage() {
   return <ScanForm />;
